7540d3fbab7c4d4bd0bad06d70edd479928091e7
hashmaniac <mbithidenis3@gmail.com>
2022-10-07 19:39:52 +0300

    create account

---
+++ create-account.php
@@ -0,0 +1,13 @@
+<?php
+//1. Create account
+
+//INIT
+require 'app/config.php';
+require PATH_LIB . 'lib.php';
+$libDB = new DB();
+
+$pass = $libDb->exec(
+    "INSERT INTO `users` (`username`, `email`, `password`) VALUES (?, ?, ?)", [`johndoe`,`john@example.com`,`12345678`]
+);
+
+echo $pass ? "Ok" : $libDB->error;
